d5a688fdc8b7e22e3cde340c15f4540a4ac30bf9
nearKim <cheshire0814@gmail.com>
2018-03-02 19:24:55 +0900

    Add models for accounts app. Add category.py for additional variables used in models.

--- accounts/models.py
+++ accounts/models.py
@@ -3,52 +3,55 @@ from django.contrib.auth.models import User
 from imagekit.models import ProcessedImageField
 from imagekit.processors import Thumbnail
 
+from .category import *
+
+
 class Profile(models.Model):
-    DEPARTMENT_CATEGORY = (
-        ('hum', '인문대학'),
-        ('soc', '사회과학대학'),
-        ('nat', '자연과학대학'),
-        ('nur', '간호대학'),
-        ('mba', '경영대학'),
-        ('eng', '공과대학'),
-        ('agr', '농업생명과학대학'),
-        ('art', '미술대학'),
-        ('law', '법과대학'),
-        ('edu', '사범대학'),
-        ('che', '생활과학대학'),
-        ('vet', '수의과대학'),
-        ('pha', '약학대학'),
-        ('mus', '음악대학'),
-        ('med', '의과대학'),
-        ('cls', '자유전공학부'),
-        ('uni', '연합전공'),
-        ('cor', '연계전공'),
-    )
-
-    STUDENT_CATEGORY = (
-        ('u', '학부'),
-        ('g', '대학원'),
-        ('i', '교환학생'),
-    )
-    SEMESTER_CATEGORY = (
-        ('s', '1학기'),
-        ('f', '2학기')
-    )
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30, null=False, blank=False)
-    birth_date = models.DateField(null=False, blank=False)
-    phone = models.CharField(max_length=20, blank=False, null=False)
-    student_no = models.CharField(max_length=12, blank=False, null=False)
-    college = models.CharField(max_length=3, choices=DEPARTMENT_CATEGORY, null=False, blank=False)
-    # department =
-    student_category = models.CharField(max_length=1, choices=STUDENT_CATEGORY, null=False, blank=False)
-    enroll_year = models.DateField(auto_now_add=True)
-    enroll_semester = models.CharField(max_length=1, choices=SEMESTER_CATEGORY, null=False, blank=False)
+    name = models.CharField(max_length=30, null=False, blank=False, verbose_name='이름')
+    birth_date = models.DateField(null=False, blank=False, verbose_name='생년월일')
+    phone = models.CharField(max_length=20, blank=False, null=False, verbose_name='전화번호')
+    student_no = models.CharField(max_length=12, blank=False, null=False, verbose_name='학번')
+    college = models.CharField(max_length=3, choices=COLLEGE_CATEGORY, null=False, blank=False, verbose_name='단과대학')
+    department = models.CharField(max_length=2, choices=DEPARTMENT_CATEGORY, null=False, blank=False,verbose_name='학과')
+    student_category = models.CharField(max_length=1, choices=STUDENT_CATEGORY, null=False, blank=False,verbose_name='학생 유형')
+    enroll_year = models.DateField(auto_now_add=True,verbose_name='가입년도')
+    enroll_semester = models.CharField(max_length=1, choices=SEMESTER_CATEGORY, null=False, blank=False, verbose_name='가입학기')
     profile_pic = ProcessedImageField(blank=True, upload_to='profile_pic',
-                                      processors=[Thumbnail(300,300)],
+                                      processors=[Thumbnail(300, 300)],
                                       format='JPEG',
                                       options={'quality': 60})
+    is_first_paid = models.BooleanField(default=False, blank=False, null=False, verbose_name='최초입금확인')
+
+    # Default Data
+    create_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+
+class ActiveUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    last_active_year = models.DateField(auto_now=True,verbose_name='최종활동년도')
+    last_active_semester = models.CharField(max_length=1, choices=SEMESTER_CATEGORY, null=False, blank=False,verbose_name='최종활동학기')
+    is_new = models.BooleanField(default=False, blank=False, null=False,verbose_name='신입회원여부')
+    is_paid = models.BooleanField(default=False, blank=False, null=False,verbose_name='입금확인')
+
+    # Default Data
+    create_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Partners(models.Model):
+    partner_year = models.DateField(null=False, blank=False, verbose_name='짝지 년도')
+    partner_semester = models.CharField(max_length=1, choices=SEMESTER_CATEGORY, null=False, blank=False, verbose_name='짝지 학기')
+    old_partner = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='위짝지')
+    new_partner1 = models.OneToOneField(User, on_delete=models.SET_NULL, default=None, verbose_name='아래짝지1')
+    new_partner2 = models.OneToOneField(User, on_delete=models.SET_NULL, default=None, verbose_name='아래짝지2')
+    new_partner3 = models.OneToOneField(User, on_delete=models.SET_NULL, default=None, verbose_name='아래짝지3')
+    score = models.IntegerField(default=0, verbose_name='점수')
+
+    # Default Data
+    create_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
